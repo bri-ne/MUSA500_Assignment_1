f578fc5412043ce62a0ad0d1dd9f700e3503fcc7
bri-ne <89992561+bri-ne@users.noreply.github.com>
2021-10-05 13:14:42 -0400

    rstudio

---
+++ assignment1.R
@@ -0,0 +1,18 @@
+#--- load libraries ----
+install.packages("DAAG")
+install.packages("car")
+install.packages("MASS")
+install.packages("rsq")
+library(tidyr)
+library(dplyr)
+library(DAAG)
+library(car)  #to calculate VIF
+library(MASS)
+library(rsq)
+
+#---- Upload Data ----
+
+ourdata <- read.csv("https://raw.githubusercontent.com/bri-ne/MUSA500_Assignment_1/main/RegressionData.csv")
+
+
+
